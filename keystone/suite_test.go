c8ef8e9db4c8e86ba8434e1b0359fee584989c16
Francisco Souza <fss@corp.globo.com>
2012-08-30 10:49:26 -0300

    keystone: move http test server to its own package (to use within nova)

--- keystone/suite_test.go
+++ keystone/suite_test.go
@@ -1,12 +1,10 @@
 package keystone
 
 import (
+	os_testing "github.com/timeredbull/openstack/testing"
 	"io/ioutil"
 	. "launchpad.net/gocheck"
-	"net/http"
-	"net/url"
 	"testing"
-	"time"
 )
 
 var _ = Suite(&S{})
@@ -17,7 +15,7 @@ type S struct {
 
 func Test(t *testing.T) { TestingT(t) }
 
-var testServer = NewTestHTTPServer("http://localhost:4444")
+var testServer = os_testing.NewTestHTTPServer("http://localhost:4444")
 
 func (s *S) SetUpSuite(c *C) {
 	testServer.Start()
@@ -28,71 +26,4 @@ func (s *S) SetUpSuite(c *C) {
 
 func (s *S) TearDownTest(c *C) {
 	testServer.FlushRequests()
-}
-
-type TestHTTPServer struct {
-	URL      string
-	started  bool
-	request  chan *http.Request
-	response chan *testResponse
-}
-
-type testResponse struct {
-	Status  int
-	Headers map[string]string
-	Body    string
-}
-
-func NewTestHTTPServer(url string) *TestHTTPServer {
-	return &TestHTTPServer{URL: url}
-}
-
-func (s *TestHTTPServer) Start() {
-	if s.started {
-		return
-	}
-	s.started = true
-	s.request = make(chan *http.Request, 64)
-	s.response = make(chan *testResponse, 64)
-	url, _ := url.Parse(s.URL)
-	go http.ListenAndServe(url.Host, s)
-	s.PrepareResponse(202, nil, "Nothing.")
-	for {
-		resp, err := http.Get(s.URL)
-		if err == nil && resp.StatusCode == 202 {
-			break
-		}
-		time.Sleep(1e8)
-	}
-	s.WaitRequest()
-}
-
-func (s *TestHTTPServer) FlushRequests() {
-	for {
-		select {
-		case <-s.request:
-		default:
-			return
-		}
-	}
-}
-
-func (s *TestHTTPServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	s.request <- req
-	var resp *testResponse
-	resp = <-s.response
-	if resp.Status != 0 {
-		w.WriteHeader(resp.Status)
-	}
-	w.Write([]byte(resp.Body))
-}
-
-func (s *TestHTTPServer) WaitRequest() *http.Request {
-	req := <-s.request
-	req.ParseForm()
-	return req
-}
-
-func (s *TestHTTPServer) PrepareResponse(status int, headers map[string]string, body string) {
-	s.response <- &testResponse{status, headers, body}
 }
---
+++ testing/http_server.go
@@ -0,0 +1,74 @@
+package testing
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type TestHTTPServer struct {
+	URL      string
+	started  bool
+	request  chan *http.Request
+	response chan *testResponse
+}
+
+type testResponse struct {
+	Status  int
+	Headers map[string]string
+	Body    string
+}
+
+func NewTestHTTPServer(url string) *TestHTTPServer {
+	return &TestHTTPServer{URL: url}
+}
+
+func (s *TestHTTPServer) Start() {
+	if s.started {
+		return
+	}
+	s.started = true
+	s.request = make(chan *http.Request, 64)
+	s.response = make(chan *testResponse, 64)
+	url, _ := url.Parse(s.URL)
+	go http.ListenAndServe(url.Host, s)
+	s.PrepareResponse(202, nil, "Nothing.")
+	for {
+		resp, err := http.Get(s.URL)
+		if err == nil && resp.StatusCode == 202 {
+			break
+		}
+		time.Sleep(1e8)
+	}
+	s.WaitRequest()
+}
+
+func (s *TestHTTPServer) FlushRequests() {
+	for {
+		select {
+		case <-s.request:
+		default:
+			return
+		}
+	}
+}
+
+func (s *TestHTTPServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	s.request <- req
+	var resp *testResponse
+	resp = <-s.response
+	if resp.Status != 0 {
+		w.WriteHeader(resp.Status)
+	}
+	w.Write([]byte(resp.Body))
+}
+
+func (s *TestHTTPServer) WaitRequest() *http.Request {
+	req := <-s.request
+	req.ParseForm()
+	return req
+}
+
+func (s *TestHTTPServer) PrepareResponse(status int, headers map[string]string, body string) {
+	s.response <- &testResponse{status, headers, body}
+}
