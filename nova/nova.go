f3cced0f1b22dfb6ce0bf117af286d631df8f75a
Francisco Souza <fss@corp.globo.com>
2012-08-30 15:02:36 -0300

    nova: first version of the package with the DisassociateNetwork method
    This method disassociate a network from a tenant.

---
+++ nova/nova.go
@@ -0,0 +1,83 @@
+package nova
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/timeredbull/openstack/keystone"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type NetworkDisassociator interface {
+	DisassociateNetwork(tenantId string) error
+}
+
+type network struct {
+	Id       string
+	TenantId string `json:"project_id"`
+}
+
+type Client struct {
+	KeystoneClient *keystone.Client
+}
+
+func (c *Client) do(req *http.Request) ([]byte, int, error) {
+	req.Header.Set("X-Auth-Token", c.KeystoneClient.Token)
+	if req.Body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	return b, resp.StatusCode, err
+}
+
+func (c *Client) DisassociateNetwork(tenantId string) error {
+	if c.KeystoneClient == nil {
+		return errors.New("KeystoneClient is nil.")
+	}
+	endpoint := c.KeystoneClient.Endpoint("compute", "admin")
+	req, err := http.NewRequest("GET", endpoint+"/os-networks", nil)
+	if err != nil {
+		return err
+	}
+	body, status, err := c.do(req)
+	if err != nil {
+		return errors.New("Failed to get the list of all networks: " + err.Error())
+	}
+	if status != http.StatusOK {
+		return fmt.Errorf("Failed to get the list of all networks, status: %d.\nBody: %s.", status, body)
+	}
+	var result map[string][]network
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return errors.New("Failed to get the list of all networks, the server did not respond a valid JSON.")
+	}
+	var netId string
+	for _, net := range result["networks"] {
+		if net.TenantId == tenantId {
+			netId = net.Id
+			break
+		}
+	}
+	// TODO(fsouza): return error if the tenant does not have any network (netId == "").
+	reqBody := strings.NewReader(`{"disassociate":null}`)
+	req, err = http.NewRequest("POST", endpoint+"/os-networks/"+netId+"/action", reqBody)
+	req.Header.Set("Accept", "application/json")
+	if err != nil {
+		return err
+	}
+	body, status, err = c.do(req)
+	if err != nil {
+		return fmt.Errorf("Failed to disassociate the network %s from the tenant %s: %s", netId, tenantId, err)
+	}
+	if status != http.StatusAccepted {
+		return fmt.Errorf("Failed to disassociate the network %s from the tenant %s, status: %d.\nBody: %s", netId, tenantId, status, body)
+	}
+	return nil
+}
---
+++ nova/nova_test.go
@@ -0,0 +1,73 @@
+package nova
+
+import (
+	"github.com/timeredbull/openstack/keystone"
+	ostesting "github.com/timeredbull/openstack/testing"
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+type S struct{}
+
+var _ = Suite(&S{})
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+var testServer = ostesting.NewTestHTTPServer("http://localhost:5555")
+
+func (s *S) SetUpSuite(c *C) {
+	testServer.Start()
+}
+
+func (s *S) TearDownTest(c *C) {
+	testServer.FlushRequests()
+}
+
+func (s *S) TestDisassociateNetwork(c *C) {
+	kclient := keystone.Client{
+		Token: "123token",
+		Catalogs: []keystone.ServiceCatalog{
+			keystone.ServiceCatalog{
+				Name: "Compute Service",
+				Type: "compute",
+				Endpoints: []map[string]string{
+					map[string]string{
+						"adminURL": "http://localhost:5555/v2/123tenant",
+					},
+				},
+			},
+		},
+	}
+	body := `{"networks": [{"bridge": "br1808", "vpn_public_port": 1000, "dhcp_start": "172.25.8.3", "bridge_interface": "eth1", "updated_at": "2012-05-12 02:16:48", "id": "ef0aa0c4-48d8-4d9e-903a-61486cd60805", "cidr_v6": null, "deleted_at": null, "gateway": "172.25.8.1", "label": "private_0", "project_id": "123tenant", "vpn_private_address": "172.25.8.2", "deleted": false, "vlan": 1808, "broadcast": "172.25.8.255", "netmask": "255.255.255.0", "injected": false, "cidr": "172.25.8.0/24", "vpn_public_address": "10.170.0.14", "multi_host": true, "dns1": null, "host": null, "gateway_v6": null, "netmask_v6": null, "created_at": "2012-05-12 02:13:17"}, {"bridge": "br1808", "vpn_public_port": 1000, "dhcp_start": "172.25.8.3", "bridge_interface": "eth1", "updated_at": "2012-05-12 02:16:48", "id": "ef0aa0c5-48d8-4d9e-903a-61486cd60805", "cidr_v6": null, "deleted_at": null, "gateway": "172.25.8.1", "label": "private_0", "project_id": "1234tenant", "vpn_private_address": "172.25.8.2", "deleted": false, "vlan": 1808, "broadcast": "172.25.8.255", "netmask": "255.255.255.0", "injected": false, "cidr": "172.25.8.0/24", "vpn_public_address": "10.170.0.14", "multi_host": true, "dns1": null, "host": null, "gateway_v6": null, "netmask_v6": null, "created_at": "2012-05-12 02:13:17"}]}`
+	testServer.PrepareResponse(200, map[string]string{"Content-Type": "application/json"}, body) // List networks
+	testServer.PrepareResponse(202, nil, "")                                                     // Disassociate network
+	client := Client{KeystoneClient: &kclient}
+	err := client.DisassociateNetwork("123tenant")
+	c.Assert(err, IsNil)
+	listreq, _, err := testServer.WaitRequest(1e9)
+	if err != nil {
+		c.Error("Did not send the request to retrieve the list of networks after 1 second.")
+		c.FailNow()
+	}
+	disreq, b, err := testServer.WaitRequest(1e9)
+	if err != nil {
+		c.Error("Did not send the request to disassociate the network after 1 second.")
+		c.FailNow()
+	}
+	c.Assert(err, IsNil)
+	c.Assert(listreq.URL.Path, Equals, "/v2/123tenant/os-networks")
+	c.Assert(listreq.Header.Get("X-Auth-Token"), Equals, "123token")
+	c.Assert(disreq.URL.Path, Equals, "/v2/123tenant/os-networks/ef0aa0c4-48d8-4d9e-903a-61486cd60805/action")
+	c.Assert(disreq.Header.Get("X-Auth-Token"), Equals, "123token")
+	c.Assert(disreq.Header.Get("Content-Type"), Equals, "application/json")
+	c.Assert(disreq.Header.Get("Accept"), Equals, "application/json")
+	c.Assert(string(b), Equals, `{"disassociate":null}`)
+}
+
+func (s *S) TestDisassociateNetworkWithoutKeystoneClient(c *C) {
+	cli := Client{}
+	err := cli.DisassociateNetwork("anything")
+	c.Assert(err, NotNil)
+}
